80df9c5d10aca520091721712499793506d9ea15
Hung Nguyen <hungnq131002@gmail.com>
2023-11-15 22:40:28 +0700

    refactor: using llama index source instead of langchain

---
+++ src/rag/entity/base_reader.py
@@ -0,0 +1,24 @@
+"""Base reader class."""
+from abc import ABC
+from typing import Any, Iterable, List
+
+from rag.bridge.langchain import Document as LCDocument
+from rag.entity.schema import Document
+
+class BaseReader(ABC):
+    """Utilities for loading data from a directory."""
+
+    def lazy_load_data(self, *args: Any, **load_kwargs: Any) -> Iterable[Document]:
+        """Load data from the input directory lazily."""
+        raise NotImplementedError(
+            f"{self.__class__.__name__} does not provide lazy_load_data method currently"
+        )
+
+    def load_data(self, *args: Any, **load_kwargs: Any) -> List[Document]:
+        """Load data from the input directory."""
+        return list(self.lazy_load_data(*args, **load_kwargs))
+
+    def load_langchain_documents(self, **load_kwargs: Any) -> List[LCDocument]:
+        """Load data in LangChain document format."""
+        docs = self.load_data(**load_kwargs)
+        return [d.to_langchain_format() for d in docs]
