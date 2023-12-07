30d4c02a56fa2145e2cb5c2c522bb4ce39d46012
Hung Nguyen <hungnq131002@gmail.com>
2023-12-07 13:01:59 +0700

    feat: add base vector store

---
+++ src/rag/entity/base_vector.py
@@ -0,0 +1,80 @@
+
+"""Vector store index types."""
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from enum import Enum
+from typing import (
+    Any,
+    List,
+    Optional,
+    Protocol,
+    runtime_checkable,
+)
+import fsspec
+
+from llama_index.vector_stores.types import VectorStoreQuery, VectorStoreQueryResult
+from .schema import BaseNode
+
+@runtime_checkable
+class VectorStore(Protocol):
+    """Abstract vector store protocol."""
+
+    stores_text: bool
+    is_embedding_query: bool = True
+
+    @property
+    def client(self) -> Any:
+        """Get client."""
+        ...
+
+    def add(
+        self,
+        nodes: List[BaseNode],
+        **add_kwargs: Any,
+    ) -> List[str]:
+        """Add nodes with embedding to vector store."""
+        ...
+
+    async def async_add(
+        self,
+        nodes: List[BaseNode],
+        **kwargs: Any,
+    ) -> List[str]:
+        """
+        Asynchronously add nodes with embedding to vector store.
+        NOTE: this is not implemented for all vector stores. If not implemented,
+        it will just call add synchronously.
+        """
+        return self.add(nodes)
+
+    def delete(self, ref_doc_id: str, **delete_kwargs: Any) -> None:
+        """
+        Delete nodes using with ref_doc_id."""
+        ...
+
+    async def adelete(self, ref_doc_id: str, **delete_kwargs: Any) -> None:
+        """
+        Delete nodes using with ref_doc_id.
+        NOTE: this is not implemented for all vector stores. If not implemented,
+        it will just call delete synchronously.
+        """
+        self.delete(ref_doc_id, **delete_kwargs)
+
+    def query(self, query: VectorStoreQuery, **kwargs: Any) -> VectorStoreQueryResult:
+        """Query vector store."""
+        ...
+
+    async def aquery(
+        self, query: VectorStoreQuery, **kwargs: Any
+    ) -> VectorStoreQueryResult:
+        """
+        Asynchronously query vector store.
+        NOTE: this is not implemented for all vector stores. If not implemented,
+        it will just call query synchronously.
+        """
+        return self.query(query, **kwargs)
+
+    def persist(
+        self, persist_path: str, fs: Optional[fsspec.AbstractFileSystem] = None
+    ) -> None:
+        return None
