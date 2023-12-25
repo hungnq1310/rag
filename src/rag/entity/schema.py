e5d6ab91b0aa3d34d73d2a66f23f41aff93a0908
Hung Nguyen <hungnq131002@gmail.com>
2023-12-25 20:00:08 +0700

    refactor: detach schema

--- src/rag/entity/schema.py
+++ src/rag/entity/schema.py
@@ -1,494 +1,159 @@
 """Base schema for data structures."""
 import json
-import textwrap
-import uuid
 from abc import abstractmethod
-from enum import Enum, auto
-from hashlib import sha256
-from dataclasses import dataclass
-from typing import Any, Dict, List, Optional, Union
+from abc import ABC, abstractmethod
+from enum import Enum
+from typing import (
+    Any,
+    AsyncGenerator,
+    Generator,
+    Generic,
+    List,
+    Protocol,
+    Type,
+    TypeVar,
+    Union,
+    runtime_checkable,
+    Dict
+)
 
-from dataclasses_json import DataClassJsonMixin
+from rag.bridge.pydantic import BaseModel
+from rag.entity.llm.base_llm import ChatMessage, MessageRole
 from typing_extensions import Self
 
-from rag.bridge.pydantic import BaseModel, Field
-from rag.utils.utils import SAMPLE_TEXT, truncate_text
 
-DEFAULT_TEXT_NODE_TMPL = "{metadata_str}\n\n{content}"
-DEFAULT_METADATA_TMPL = "{key}: {value}"
-# NOTE: for pretty printing
-TRUNCATE_LENGTH = 350
-WRAP_WIDTH = 70
+class BaseComponent(BaseModel):
+    """Base component object to capture class names."""
 
-
-class NodeRelationship(str, Enum):
-    """Node relationships used in `BaseNode` class.
-
-    Attributes:
-        SOURCE: The node is the source document.
-        PREVIOUS: The node is the previous node in the document.
-        NEXT: The node is the next node in the document.
-        PARENT: The node is the parent node in the document.
-        CHILD: The node is a child node in the document.
-
-    """
-
-    SOURCE = auto()
-    PREVIOUS = auto()
-    NEXT = auto()
-    PARENT = auto()
-    CHILD = auto()
-
-
-class ObjectType(str, Enum):
-    TEXT = auto()
-    IMAGE = auto()
-    INDEX = auto()
-    DOCUMENT = auto()
-
-
-class MetadataMode(str, Enum):
-    ALL = auto()
-    EMBED = auto()
-    LLM = auto()
-    NONE = auto()
-
-
-class RelatedNodeInfo(BaseModel):
-    node_id: str
-    node_type: Optional[ObjectType] = None
-    metadata: Dict[str, Any] = Field(default_factory=dict)
-    hash: Optional[str] = None
+    class Config:
+        @staticmethod
+        def schema_extra(schema: Dict[str, Any], model: "BaseComponent") -> None:
+            """Add class name to schema."""
+            schema["properties"]["class_name"] = {
+                "title": "Class Name",
+                "type": "string",
+                "default": model.class_name(),
+            }
 
     @classmethod
     def class_name(cls) -> str:
-        return "RelatedNodeInfo"
-
-
-RelatedNodeType = Union[RelatedNodeInfo, List[RelatedNodeInfo]]
-
-
-# Node classes for indexes
-class BaseNode(BaseModel):
-    """Base node Object.
-
-    Generic abstract interface for retrievable nodes
-
-    """
-
-    id_: str = Field(
-        default_factory=lambda: str(uuid.uuid4()), description="Unique ID of the node."
-    )
-    embedding: Optional[List[float]] = Field(
-        default=None, description="Embedding of the node."
-    )
-
-    """"
-    metadata fields
-    - injected as part of the text shown to LLMs as context
-    - injected as part of the text for generating embeddings
-    - used by vector DBs for metadata filtering
-
-    """
-    metadata: Dict[str, Any] = Field(
-        default_factory=dict,
-        description="A flat dictionary of metadata fields",
-        alias="extra_info",
-    )
-    excluded_embed_metadata_keys: List[str] = Field(
-        default_factory=list,
-        description="Metadata keys that are excluded from text for the embed model.",
-    )
-    excluded_llm_metadata_keys: List[str] = Field(
-        default_factory=list,
-        description="Metadata keys that are excluded from text for the LLM.",
-    )
-    relationships: Dict[NodeRelationship, RelatedNodeType] = Field(
-        default_factory=dict,
-        description="A mapping of relationships to other node information.",
-    )
-    hash: str = Field(default="", description="Hash of the node content.")
-
-    @classmethod
-    @abstractmethod
-    def get_type(cls) -> str:
-        """Get Object type."""
-
-    @abstractmethod
-    def get_content(self, metadata_mode: MetadataMode = MetadataMode.ALL) -> str:
-        """Get object content."""
-
-    @abstractmethod
-    def get_metadata_str(self, mode: MetadataMode = MetadataMode.ALL) -> str:
-        """Metadata string."""
-
-    @abstractmethod
-    def set_content(self, value: Any) -> None:
-        """Set the content of the node."""
-
-    # Properties
-    @property
-    def node_id(self):
-        return self.id_
-
-    @property
-    def source_node(self) -> Optional[RelatedNodeInfo]:
-        """Source object node.
-
-        Extracted from the relationships field.
-
         """
-        if NodeRelationship.SOURCE not in self.relationships:
-            return None
+        Get the class name, used as a unique ID in serialization.
 
-        relation = self.relationships[NodeRelationship.SOURCE]
-        if isinstance(relation, list):
-            raise ValueError("Source object must be a single RelatedNodeInfo object")
-        return relation
-
-    @property
-    def prev_node(self) -> Optional[RelatedNodeInfo]:
-        """Prev node."""
-        if NodeRelationship.PREVIOUS not in self.relationships:
-            return None
-
-        relation = self.relationships[NodeRelationship.PREVIOUS]
-        if not isinstance(relation, RelatedNodeInfo):
-            raise ValueError("Previous object must be a single RelatedNodeInfo object")
-        return relation
-
-    @property
-    def next_node(self) -> Optional[RelatedNodeInfo]:
-        """Next node."""
-        if NodeRelationship.NEXT not in self.relationships:
-            return None
-
-        relation = self.relationships[NodeRelationship.NEXT]
-        if not isinstance(relation, RelatedNodeInfo):
-            raise ValueError("Next object must be a single RelatedNodeInfo object")
-        return relation
-
-    @property
-    def parent_node(self) -> Optional[RelatedNodeInfo]:
-        """Parent node."""
-        if NodeRelationship.PARENT not in self.relationships:
-            return None
-
-        relation = self.relationships[NodeRelationship.PARENT]
-        if not isinstance(relation, RelatedNodeInfo):
-            raise ValueError("Parent object must be a single RelatedNodeInfo object")
-        return relation
-
-    @property
-    def child_nodes(self) -> Optional[List[RelatedNodeInfo]]:
-        """Child nodes."""
-        if NodeRelationship.CHILD not in self.relationships:
-            return None
-
-        relation = self.relationships[NodeRelationship.CHILD]
-        if not isinstance(relation, list):
-            raise ValueError("Child objects must be a list of RelatedNodeInfo objects.")
-        return relation
-
-    def get_embedding(self) -> List[float]:
-        """Get embedding.
-
-        Errors if embedding is None.
-
+        This provides a key that makes serialization robust against actual class
+        name changes.
         """
-        if self.embedding is None:
-            raise ValueError("embedding not set.")
-        return self.embedding
+        return "base_component"
 
-    def as_related_node_info(self) -> RelatedNodeInfo:
-        """Get node as RelatedNodeInfo."""
-        return RelatedNodeInfo(
-            node_id=self.node_id,
-            node_type=self.get_type(),
-            metadata=self.metadata,
-            hash=self.hash,
-        )
+    def json(self, **kwargs: Any) -> str:
+        return self.to_json(**kwargs)
 
+    def dict(self, **kwargs: Any) -> Dict[str, Any]:
+        data = super().dict(**kwargs)
+        data["class_name"] = self.class_name()
+        return data
 
-class TextNode(BaseNode):
-    text: str = Field(default="", description="Text content of the node.")
-    start_char_idx: Optional[int] = Field(
-        default=None, description="Start char index of the node."
-    )
-    end_char_idx: Optional[int] = Field(
-        default=None, description="End char index of the node."
-    )
-    text_template: str = Field(
-        default=DEFAULT_TEXT_NODE_TMPL,
-        description=(
-            "Template for how text is formatted, with {content} and "
-            "{metadata_str} placeholders."
-        ),
-    )
-    metadata_template: str = Field(
-        default=DEFAULT_METADATA_TMPL,
-        description=(
-            "Template for how metadata is formatted, with {key} and "
-            "{value} placeholders."
-        ),
-    )
-    metadata_seperator: str = Field(
-        default="\n",
-        description="Separator between metadata fields when converting to string.",
-    )
+    def __getstate__(self) -> Dict[str, Any]:
+        state = super().__getstate__()
+
+        # tiktoken is not pickleable
+        state["__dict__"].pop("tokenizer", None)
+
+        # remove local functions
+        keys_to_remove = []
+        for key in state["__dict__"]:
+            if key.endswith("_fn"):
+                keys_to_remove.append(key)
+        for key in keys_to_remove:
+            state["__dict__"].pop(key, None)
+
+        # remove private attributes
+        state["__private_attribute_values__"] = {}
+
+        return state
+
+    def to_dict(self, **kwargs: Any) -> Dict[str, Any]:
+        data = self.dict(**kwargs)
+        data["class_name"] = self.class_name()
+        return data
+
+    def to_json(self, **kwargs: Any) -> str:
+        data = self.to_dict(**kwargs)
+        return json.dumps(data)
+
+    # TODO: return type here not supported by current mypy version
+    @classmethod
+    def from_dict(cls, data: Dict[str, Any], **kwargs: Any) -> Self:  # type: ignore
+        if isinstance(kwargs, dict):
+            data.update(kwargs)
+
+        data.pop("class_name", None)
+        return cls(**data)
 
     @classmethod
-    def class_name(cls) -> str:
-        return "TextNode"
-
-    # TODO: find when generating hash of nodebase or relatedInfoNode??
-    @classmethod
-    def _generate_hash(cls, values: dict) -> dict:
-        """Generate a hash to represent the node."""
-        text = values.get("text", "")
-        metadata = values.get("metadata", {})
-        doc_identity = str(text) + str(metadata)
-        values["hash"] = str(
-            sha256(doc_identity.encode("utf-8", "surrogatepass")).hexdigest()
-        )
-        return values
-
-    # Implement abstract methods
-    @classmethod
-    def get_type(cls) -> str:
-        """Get Object type."""
-        return ObjectType.TEXT
-
-    def get_content(self, metadata_mode: MetadataMode = MetadataMode.NONE) -> str:
-        """Get object content."""
-        metadata_str = self.get_metadata_str(mode=metadata_mode).strip()
-        if not metadata_str:
-            return self.text
-
-        return self.text_template.format(
-            content=self.text, metadata_str=metadata_str
-        ).strip()
-
-    def get_metadata_str(self, mode: MetadataMode = MetadataMode.ALL) -> str:
-        """Metadata info string."""
-        if mode == MetadataMode.NONE:
-            return ""
-
-        usable_metadata_keys = set(self.metadata.keys())
-        if mode == MetadataMode.LLM:
-            for key in self.excluded_llm_metadata_keys:
-                if key in usable_metadata_keys:
-                    usable_metadata_keys.remove(key)
-        elif mode == MetadataMode.EMBED:
-            for key in self.excluded_embed_metadata_keys:
-                if key in usable_metadata_keys:
-                    usable_metadata_keys.remove(key)
-
-        return self.metadata_seperator.join(
-            [
-                self.metadata_template.format(key=key, value=str(value))
-                for key, value in self.metadata.items()
-                if key in usable_metadata_keys
-            ]
-        )
-
-    def set_content(self, value: str) -> None:
-        """Set the content of the node."""
-        self.text = value
-
-    def get_node_info(self) -> Dict[str, Any]:
-        """Get node info."""
-        return {"start": self.start_char_idx, "end": self.end_char_idx}
-
-    def get_text(self) -> str:
-        return self.get_content(metadata_mode=MetadataMode.NONE)
-
-    @property
-    def node_info(self) -> Dict[str, Any]:
-        """Deprecated: Get node info."""
-        return self.get_node_info()
+    def from_json(cls, data_str: str, **kwargs: Any) -> Self:  # type: ignore
+        data = json.loads(data_str)
+        return cls.from_dict(data, **kwargs)
 
 
-# TODO: legacy backport of old Node class
-Node = TextNode
+Model = TypeVar("Model", bound=BaseModel)
 
-class IndexNode(TextNode):
-    """Node with reference to any object.
-
-    This can include other indices, query engines, retrievers.
-
-    This can also include other nodes (though this is overlapping with `relationships`
-    on the Node class).
-    """
-
-    index_id: str
-
-    @classmethod
-    def from_text_node(
-        cls,
-        node: TextNode,
-        index_id: str,
-    ) -> "IndexNode":
-        """Create index node from text node."""
-        # copy all attributes from text node, add index id
-        return cls(
-            **node.dict(),
-            index_id=index_id,
-        )
-
-    @classmethod
-    def get_type(cls) -> str:
-        return ObjectType.INDEX
-
-    @classmethod
-    def class_name(cls) -> str:
-        return "IndexNode"
+TokenGen = Generator[str, None, None]
+TokenAsyncGen = AsyncGenerator[str, None]
+RESPONSE_TEXT_TYPE = Union[BaseModel, str, TokenGen]
 
 
-class NodeWithScore(BaseModel):
-    node: BaseNode
-    score: Optional[float] = None
+# TODO: move into a `core` folder
+# NOTE: this is necessary to make it compatible with pydantic
+@runtime_checkable
+class BaseOutputParser(Protocol):
+    """Output parser class."""
 
-    def __str__(self) -> str:
-        return f"{self.node}\nScore: {self.score: 0.3f}\n"
+    @abstractmethod
+    def parse(self, output: str) -> Any:
+        """Parse, validate, and correct errors programmatically."""
 
-    def get_score(self, raise_error: bool = False) -> float:
-        """Get score."""
-        if self.score is None:
-            if raise_error:
-                raise ValueError("Score not set.")
+    @abstractmethod
+    def format(self, query: str) -> str:
+        """Format a query with structured output formatting instructions."""
+
+    def format_messages(self, messages: List[ChatMessage]) -> List[ChatMessage]:
+        """Format a list of messages with structured output formatting instructions."""
+        # NOTE: apply output parser to either the first message if it's a system message
+        #       or the last message
+        if messages:
+            if messages[0].role == MessageRole.SYSTEM:
+                messages[0].content = self.format(messages[0].content or "")
             else:
-                return 0.0
-        else:
-            return self.score
+                messages[-1].content = self.format(messages[-1].content or "")
 
-    @classmethod
-    def class_name(cls) -> str:
-        return "NodeWithScore"
-
-    ##### pass through methods to BaseNode #####
-    @property
-    def node_id(self) -> str:
-        return self.node.node_id
-
-    @property
-    def id_(self) -> str:
-        return self.node.id_
-
-    @property
-    def text(self) -> str:
-        if isinstance(self.node, TextNode):
-            return self.node.text
-        else:
-            raise ValueError("Node must be a TextNode to get text.")
-
-    @property
-    def metadata(self) -> Dict[str, Any]:
-        return self.node.metadata
-
-    @property
-    def embedding(self) -> Optional[List[float]]:
-        return self.node.embedding
-
-    def get_text(self) -> str:
-        if isinstance(self.node, TextNode):
-            return self.node.get_text()
-        else:
-            raise ValueError("Node must be a TextNode to get text.")
-
-    def get_content(self, metadata_mode: MetadataMode = MetadataMode.NONE) -> str:
-        return self.node.get_content(metadata_mode=metadata_mode)
-
-    def get_embedding(self) -> List[float]:
-        return self.node.get_embedding()
+        return messages
 
 
-# Document Classes for Readers
+class BasePydanticProgram(ABC, Generic[Model]):
+    """A base class for LLM-powered function that return a pydantic model.
 
-class Document(TextNode):
-    """Generic interface for a data document.
-
-    This document connects to data sources.
-
+    Note: this interface is not yet stable.
     """
 
-    # TODO: A lot of backwards compatibility logic here, clean up
-    id_: str = Field(
-        default_factory=lambda: str(uuid.uuid4()),
-        description="Unique ID of the node.",
-        alias="doc_id",
-    )
-
-    _compat_fields = {"doc_id": "id_", "extra_info": "metadata"}
-
-    @classmethod
-    def get_type(cls) -> str:
-        """Get Document type."""
-        return ObjectType.DOCUMENT
-
     @property
-    def doc_id(self) -> str:
-        """Get document ID."""
-        return self.id_
+    @abstractmethod
+    def output_cls(self) -> Type[Model]:
+        pass
 
-    def __str__(self) -> str:
-        source_text_truncated = truncate_text(
-            self.get_content().strip(), TRUNCATE_LENGTH
-        )
-        source_text_wrapped = textwrap.fill(
-            f"Text: {source_text_truncated}\n", width=WRAP_WIDTH
-        )
-        return f"Doc ID: {self.doc_id}\n{source_text_wrapped}"
+    @abstractmethod
+    def __call__(self, *args: Any, **kwds: Any) -> Model:
+        pass
 
-    def get_doc_id(self) -> str:
-        """TODO: Deprecated: Get document ID."""
-        return self.id_
-
-    def __setattr__(self, name: str, value: object) -> None:
-        if name in self._compat_fields:
-            name = self._compat_fields[name]
-        super().__setattr__(name, value)
-
-    @classmethod
-    def example(cls) -> "Document":
-        return Document(
-            text=SAMPLE_TEXT,
-            metadata={"filename": "README.md", "category": "codebase"},
-        )
-
-    @classmethod
-    def class_name(cls) -> str:
-        return "Document"
+    async def acall(self, *args: Any, **kwds: Any) -> Model:
+        return self(*args, **kwds)
 
 
-@dataclass
-class QueryBundle(DataClassJsonMixin):
-    """
-    Query bundle.
+class PydanticProgramMode(str, Enum):
+    """Pydantic program mode."""
 
-    This dataclass contains the original query string and associated transformations.
-
-    Args:
-        query_str (str): the original user-specified query string.
-            This is currently used by all non embedding-based queries.
-        embedding_strs (list[str]): list of strings used for embedding the query.
-            This is currently used by all embedding-based queries.
-        embedding (list[float]): the stored embedding for the query.
-    """
-
-    query_str: str
-    custom_embedding_strs: Optional[List[str]] = None
-    embedding: Optional[List[float]] = None
-
-    @property
-    def embedding_strs(self) -> List[str]:
-        """Use custom embedding strs if specified, otherwise use query str."""
-        if self.custom_embedding_strs is None:
-            if len(self.query_str) == 0:
-                return []
-            return [self.query_str]
-        else:
-            return self.custom_embedding_strs
-
-
-QueryType = Union[str, QueryBundle]
+    DEFAULT = "default"
+    OPENAI = "openai"
+    LLM = "llm"
+    GUIDANCE = "guidance"
+    LM_FORMAT_ENFORCER = "lm-format-enforcer"
