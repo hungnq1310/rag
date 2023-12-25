b186ca06b4c281e81ff9d86ccb60adbc63d23f07
Hung Nguyen <hungnq131002@gmail.com>
2023-12-25 19:53:10 +0700

    refactor: detach node base for data structure

---
+++ src/rag/entity/node/__init__.py
@@ -0,0 +1,58 @@
+"""Base schema for data structures."""
+from enum import Enum, auto
+from typing import Any, Dict, List, Optional, Union
+
+from rag.bridge.pydantic import BaseModel, Field
+
+DEFAULT_TEXT_NODE_TMPL = "{metadata_str}\n\n{content}"
+DEFAULT_METADATA_TMPL = "{key}: {value}"
+# NOTE: for pretty printing
+TRUNCATE_LENGTH = 350
+WRAP_WIDTH = 70
+
+class NodeRelationship(str, Enum):
+    """Node relationships used in `BaseNode` class.
+
+    Attributes:
+        SOURCE: The node is the source document.
+        PREVIOUS: The node is the previous node in the document.
+        NEXT: The node is the next node in the document.
+        PARENT: The node is the parent node in the document.
+        CHILD: The node is a child node in the document.
+
+    """
+
+    SOURCE = auto()
+    PREVIOUS = auto()
+    NEXT = auto()
+    PARENT = auto()
+    CHILD = auto()
+
+
+class ObjectType(str, Enum):
+    TEXT = auto()
+    IMAGE = auto()
+    INDEX = auto()
+    DOCUMENT = auto()
+
+
+class MetadataMode(str, Enum):
+    ALL = auto()
+    EMBED = auto()
+    LLM = auto()
+    NONE = auto()
+
+
+class RelatedNodeInfo(BaseModel):
+    node_id: str
+    node_type: Optional[ObjectType] = None
+    metadata: Dict[str, Any] = Field(default_factory=dict)
+    hash: Optional[str] = None
+
+    @classmethod
+    def class_name(cls) -> str:
+        return "RelatedNodeInfo"
+
+
+RelatedNodeType = Union[RelatedNodeInfo, List[RelatedNodeInfo]]
+
