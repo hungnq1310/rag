8d2c603b8c0579c5c732d9a8f6278dd55e2a3881
Hung Nguyen <hungnq131002@gmail.com>
2023-12-26 10:35:02 +0700

    refactor: detach dependices and schema

--- src/rag/entity/node/base_node.py
+++ src/rag/entity/node/base_node.py
@@ -3,9 +3,8 @@ import textwrap
 import uuid
 from abc import abstractmethod
 from hashlib import sha256
-from typing import Any, Dict, List, Optional, Union
-from enum import Enum, auto
-from rag.bridge.pydantic import BaseModel, Field
+
+from . import *
 from rag.utils.utils import SAMPLE_TEXT, truncate_text
 
 DEFAULT_TEXT_NODE_TMPL = "{metadata_str}\n\n{content}"
@@ -13,52 +12,6 @@ DEFAULT_METADATA_TMPL = "{key}: {value}"
 # NOTE: for pretty printing
 TRUNCATE_LENGTH = 350
 WRAP_WIDTH = 70
-
-class NodeRelationship(str, Enum):
-    """Node relationships used in `BaseNode` class.
-
-    Attributes:
-        SOURCE: The node is the source document.
-        PREVIOUS: The node is the previous node in the document.
-        NEXT: The node is the next node in the document.
-        PARENT: The node is the parent node in the document.
-        CHILD: The node is a child node in the document.
-
-    """
-
-    SOURCE = auto()
-    PREVIOUS = auto()
-    NEXT = auto()
-    PARENT = auto()
-    CHILD = auto()
-
-
-class ObjectType(str, Enum):
-    TEXT = auto()
-    IMAGE = auto()
-    INDEX = auto()
-    DOCUMENT = auto()
-
-
-class MetadataMode(str, Enum):
-    ALL = auto()
-    EMBED = auto()
-    LLM = auto()
-    NONE = auto()
-
-
-class RelatedNodeInfo(BaseModel):
-    node_id: str
-    node_type: Optional[ObjectType] = None
-    metadata: Dict[str, Any] = Field(default_factory=dict)
-    hash: Optional[str] = None
-
-    @classmethod
-    def class_name(cls) -> str:
-        return "RelatedNodeInfo"
-
-
-RelatedNodeType = Union[RelatedNodeInfo, List[RelatedNodeInfo]]
 
 
 # Node classes for indexes
