c5bf32b31a51383717085e85327e2c136ee8f840
Hung Nguyen <hungnq131002@gmail.com>
2023-12-27 09:08:08 +0700

    styling: import primal pandatic first

--- src/rag/bridge/pydantic.py
+++ src/rag/bridge/pydantic.py
@@ -1,18 +1,4 @@
 try:
-    from pydantic.v1 import (
-        BaseModel,
-        Field,
-        PrivateAttr,
-        StrictFloat,
-        StrictInt,
-        StrictStr,
-        create_model,
-        root_validator,
-        validator,
-    )
-    from pydantic.v1.error_wrappers import ValidationError
-    from pydantic.v1.fields import FieldInfo
-except ImportError:
     from pydantic import (
         BaseModel,
         Field,
@@ -26,7 +12,22 @@ except ImportError:
     )
     from pydantic.error_wrappers import ValidationError
     from pydantic.fields import FieldInfo
+except ImportError:
+    from pydantic.v1 import (
+        BaseModel,
+        Field,
+        PrivateAttr,
+        StrictFloat,
+        StrictInt,
+        StrictStr,
+        create_model,
+        root_validator,
+        validator,
+    )
+    from pydantic.v1.error_wrappers import ValidationError
+    from pydantic.v1.fields import FieldInfo
 
+    
 __all__ = [
     "BaseModel",
     "Field",
