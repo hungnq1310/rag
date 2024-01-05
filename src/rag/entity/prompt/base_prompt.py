d7969c4425f2d851c8be71d6b6acbcd0ac695fb9
Hung Nguyen <hungnq131002@gmail.com>
2024-01-05 14:48:21 +0700

    refactor: detach type of prompt from __init__ file

--- src/rag/entity/prompt/base_prompt.py
+++ src/rag/entity/prompt/base_prompt.py
@@ -1,8 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Any, Callable, Dict, List, Optional
 
-from rag.bridge.pydantic import BaseModel
-from rag.bridge.pydantic import Field
+from llama_index.bridge.pydantic import BaseModel, Field
 from rag.entity.schema import BaseOutputParser
 from rag.entity.llm import BaseLLM, ChatMessage
 
