df1b9279b288e89e8bbfe9053ff18cf35dfaa8f9
Hung Nguyen <hungnq131002@gmail.com>
2024-01-15 05:04:56 +0700

    chore: change name class

--- src/rag/pipeline/retriever_pipeline.py
+++ src/rag/pipeline/retriever_pipeline.py
@@ -1,33 +1,39 @@
 import logging 
 from typing import Optional, List
 
-from llama_index import VectorStoreIndex, StorageContext, ServiceContext
-from llama_index.response_synthesizers.base import BaseSynthesizer
-from llama_index.postprocessor.types import BaseNodePostprocessor
-from llama_index.retrievers import VectorIndexRetriever
-
-from rag.components.retriever_engine import RetrieverQueryEngine
+from rag.config.schema import (
+    MilvusConfig,
+    MilvusArguments,
+    IndexRetrieveParams
+)
+from rag.entity import VectorStoreIndex, StorageContext, ServiceContext
+from rag.entity.synthesizer import BaseSynthesizer
+from rag.entity.indices.vector_store import VectorIndexRetriever
+from rag.components.engine.retriever_engine import RetrieverQueryEngine
 from rag.components.vector_store.milvus import MilvusVectorStore
+from llama_index.postprocessor.types import BaseNodePostprocessor
 
 logger = logging.getLogger(__name__)
 
 class RetrieverPipeline:
     def __init__(self,
-        config,
-        params ,
+        milvus_config: MilvusConfig,
+        milvus_params: MilvusArguments,
+        index_params: IndexRetrieveParams,
         service_context: Optional[ServiceContext],
         response_synthesizer: Optional[BaseSynthesizer],
         node_postprocessors: Optional[List[BaseNodePostprocessor]],
     ) -> None:
-        self.config = config
-        self.params = params
+        self.milvus_config = milvus_config
+        self.milvus_params = milvus_params
+        self.index_params = index_params
         self.service_context = service_context
         self.response_synthesizer = response_synthesizer
         self.node_postprocessors = node_postprocessors                    
 
     def main(self, query, documents):
         #TODO: build milvus vector from documents
-        milvus_vector_store = MilvusVectorStore(self.config, self.params)
+        milvus_vector_store = MilvusVectorStore(self.milvus_config, self.milvus_params)
         # construct index and customize storage context
         storage_context = StorageContext.from_defaults(
             vector_store=milvus_vector_store
@@ -40,7 +46,7 @@ class RetrieverPipeline:
         #TODO: build retriever
         retriever = VectorIndexRetriever(
             index=index,
-            similarity_top_k=self.params.similarity_top_k,
+            similarity_top_k=self.index_params.similarity_top_k,
         )
 
         #TODO: assemble query engine
