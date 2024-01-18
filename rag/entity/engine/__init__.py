987c080594c6c14b990400ae9d1a7e7a1402307f
Hung Nguyen <hungnq131002@gmail.com>
2024-01-18 10:54:26 +0700

    chore: move base query engine to folder engine and declear absolute import

---
+++ rag/entity/engine/__init__.py
@@ -0,0 +1,5 @@
+from rag.entity.engine.base_query_engine import BaseQueryEngine
+
+__all__ = [
+    "BaseQueryEngine"
+]
---
+++ rag/entity/engine/base_query_engine.py
@@ -0,0 +1,74 @@
+"""Base query engine."""
+
+import logging
+from abc import abstractmethod
+from typing import Any, Dict, List, Optional, Sequence, TYPE_CHECKING
+
+from rag.entity.callbacks.callback_manager import CallbackManager
+from rag.entity.prompt.mixin import PromptDictType, PromptMixin
+from rag.entity.node.base_node import NodeWithScore
+from rag.entity.retriever.types import QueryBundle, QueryType
+
+if TYPE_CHECKING:
+    from rag.entity.synthesizer.types import RESPONSE_TYPE
+
+logger = logging.getLogger(__name__)
+
+
+class BaseQueryEngine(PromptMixin):
+    """Base query engine."""
+
+    def __init__(self, callback_manager: Optional[CallbackManager]) -> None:
+        self.callback_manager = callback_manager or CallbackManager([])
+
+    def _get_prompts(self) -> Dict[str, Any]:
+        """Get prompts."""
+        return {}
+
+    def _update_prompts(self, prompts: PromptDictType) -> None:
+        """Update prompts."""
+
+    def query(self, str_or_query_bundle: QueryType) -> "RESPONSE_TYPE":
+        with self.callback_manager.as_trace("query"):
+            if isinstance(str_or_query_bundle, str):
+                str_or_query_bundle = QueryBundle(str_or_query_bundle)
+            return self._query(str_or_query_bundle)
+
+    async def aquery(self, str_or_query_bundle: QueryType) -> "RESPONSE_TYPE":
+        with self.callback_manager.as_trace("query"):
+            if isinstance(str_or_query_bundle, str):
+                str_or_query_bundle = QueryBundle(str_or_query_bundle)
+            return await self._aquery(str_or_query_bundle)
+
+    def retrieve(self, query_bundle: QueryBundle) -> List[NodeWithScore]:
+        raise NotImplementedError(
+            "This query engine does not support retrieve, use query directly"
+        )
+
+    def synthesize(
+        self,
+        query_bundle: QueryBundle,
+        nodes: List[NodeWithScore],
+        additional_source_nodes: Optional[Sequence[NodeWithScore]] = None,
+    ) -> "RESPONSE_TYPE":
+        raise NotImplementedError(
+            "This query engine does not support synthesize, use query directly"
+        )
+
+    async def asynthesize(
+        self,
+        query_bundle: QueryBundle,
+        nodes: List[NodeWithScore],
+        additional_source_nodes: Optional[Sequence[NodeWithScore]] = None,
+    ) -> "RESPONSE_TYPE":
+        raise NotImplementedError(
+            "This query engine does not support asynthesize, use aquery directly"
+        )
+
+    @abstractmethod
+    def _query(self, query_bundle: QueryBundle) -> "RESPONSE_TYPE":
+        pass
+
+    @abstractmethod
+    async def _aquery(self, query_bundle: QueryBundle) -> "RESPONSE_TYPE":
+        pass
