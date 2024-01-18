758479dc66c69dcde603e0ca44efe3ab4ce6f0b6
Hung Nguyen <hungnq131002@gmail.com>
2024-01-18 14:26:50 +0700

    chore: declare import way

--- rag/components/retrievers/__init__.py
+++ rag/components/retrievers/__init__.py
@@ -1,3 +1,9 @@
-from .auto_merging import *
-from .recursive import *
-from .bm25 import *
+from rag.components.retrievers.auto_merging import AutoMergingRetriever
+from rag.components.retrievers.recursive import RecursiveRetriever
+from rag.components.retrievers.bm25 import BM25Retriever
+
+__all__ = [
+    "AutoMergingRetriever",
+    "RecursiveRetriever",
+    "BM25Retriever",
+]
