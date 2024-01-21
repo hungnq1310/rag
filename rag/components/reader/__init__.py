2cf55884654f2ddc21b5b99be0b2e74f5f3ae06d
Hung Nguyen <hungnq131002@gmail.com>
2024-01-21 11:09:05 +0700

    feat: add directory reader

--- rag/components/reader/__init__.py
+++ rag/components/reader/__init__.py
@@ -1,6 +1,8 @@
-from rag.components.reader.pdf_reader import  PDFMinerLoader, PyPDFReader
+from rag.components.reader.pdf_reader.pdf_reader import  PDFMinerLoader, PyPDFReader
+from rag.components.reader.directory_reader import DirectoryReader
 
 __all__ = [
+    "DirectoryReader",
     "PDFMinerLoader",
     "PyPDFReader"
 ]
---
+++ rag/components/reader/pdf_reader/__init__.py
@@ -0,0 +1,9 @@
+from rag.components.reader.pdf_reader.pdf_reader import  (
+    PDFMinerLoader, 
+    PyPDFReader,
+)
+
+__all__ = [
+    "PDFMinerLoader",
+    "PyPDFReader",
+]
