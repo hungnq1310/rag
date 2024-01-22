5b6d209562988bb9aaad0dc165cf487dd9fbc6d8
Hung Nguyen <hungnq131002@gmail.com>
2024-01-22 11:35:51 +0700

    fix: only parse input file path  instead of using glob to get all files again

--- rag/components/reader/file_reader/pdf_reader.py
+++ rag/components/reader/file_reader/pdf_reader.py
@@ -23,32 +23,27 @@ class PyPDFReader(BaseReader):
             raise ImportError(
                 "pypdf is required to read PDF files: `pip install pypdf`"
             )
-        # get all pdf files from directory
-        pdf_files: List[Path] = glob.glob(os.path.join(file_path, "*.pdf"))
 
-        docs = []
-        for each_file in pdf_files:
-            with open(each_file, "rb") as fp:
-                # Create a PDF object
-                pdf = pypdf.PdfReader(fp)
+        with open(file_path, "rb") as fp:
+            # Create a PDF object
+            pdf = pypdf.PdfReader(fp)
 
-                # Get the number of pages in the PDF document
-                num_pages = len(pdf.pages)
+            # Get the number of pages in the PDF document
+            num_pages = len(pdf.pages)
 
-                # Iterate over every page
-                pages_text = []
-                for page in range(num_pages):
-                    # Extract the text from the page
-                    pages_text.append(pdf.pages[page].extract_text())
-                # concatenate all pages to one long text.
-                text = "".join(pages_text)
-                # add description
-                metadata = {"num_pages": num_pages, "file_name": each_file.name}
-                if extra_info is not None:
-                    metadata.update(extra_info)
-            # apply Document class
-            docs.append(Document(text=text, metadata=metadata))
-        return docs
+            # Iterate over every page
+            pages_text = []
+            for page in range(num_pages):
+                # Extract the text from the page
+                pages_text.append(pdf.pages[page].extract_text())
+            # concatenate all pages to one long text.
+            text = "".join(pages_text)
+            # add description
+            metadata = {"num_pages": num_pages, "file_name": file_path.name}
+            if extra_info is not None:
+                metadata.update(extra_info)
+
+        return Document(text=text, metadata=metadata)
         
 
 class PDFMinerLoader(BaseReader):
@@ -65,13 +60,8 @@ class PDFMinerLoader(BaseReader):
                 "`pdfminer` package not found, please install it with "
                 "`pip install pdfminer.six`"
             )
-        # get all pdf files from directory
-        pdf_files: List[Path] = glob.glob(file_path + "/*.pdf")
-        docs = []
-        for each_file in pdf_files:
-            text = extract_text(each_file)
-            metadata = {"file_name": each_file.name}
-            if extra_info is not None:
-                metadata.update(extra_info)
-            docs.append(Document(page_content=text, metadata=metadata))
-        return docs
+        text = extract_text(file_path)
+        metadata = {"file_name": file_path.name}
+        if extra_info is not None:
+            metadata.update(extra_info)
+        return Document(page_content=text, metadata=metadata)
