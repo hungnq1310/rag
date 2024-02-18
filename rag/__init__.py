9e1e9fbf4881c7a16ca90f26c1c337bd20fbb75b
Hung Nguyen <hungnq131002@gmail.com>
2024-02-18 09:17:11 +0700

    fix: only init already inherited from base class

--- rag/__init__.py
+++ rag/__init__.py
@@ -1,142 +1,106 @@
-from rag.callbacks import CallbackManager, CBEventType, EventPayload
+from rag.cache.ingestion_cache import IngestionCache
 
-from rag.embeddings import BaseEmbedding, Pooling, load_embedding, save_embedding
-
-from rag.indices.base_index import BaseIndex
-from rag.indices.data_struct import (
-    IndexDict,
-    KeywordTable,
-    IndexList,
-    EmptyIndexStruct
+from rag.embeddings.huggingface import (
+    HuggingFaceEmbedding,
+    HuggingFaceInferenceAPIEmbedding,
+    HuggingFaceInferenceAPIEmbeddings,
 )
-from rag.indices.types import IndexStructType
+from rag.embeddings.mock import MockEmbedding
+from rag.embeddings.resovle_embed import resolve_embed_model
 
-from rag.storage.chat_store.base import BaseChatStore
-from rag.storage.docstore.base import BaseDocumentStore
-from rag.storage.index_store.base import BaseIndexStore
-from rag.storage.kv_store.base import BaseInMemoryKVStore, BaseKVStore
+from rag.llm.resolve_llm import resolve_llm
+from rag.llm.llama_cpp import LlamaCPP
+from rag.llm.huggingface import HuggingFaceInferenceAPI, HuggingFaceLLM
+from rag.llm.mock_llm import MockLLM
 
-from rag.llm.base import LLM, LLMType
-from rag.llm.generic_utils import (
-    messages_to_prompt,
-    prompt_to_messages,
-    llm_chat_callback,
-    llm_completion_callback,
+from rag.node_parser.relationship.hierarchical import HierarchicalNodeParser
+from rag.node_parser.text.sentence import SentenceSplitter
+from rag.node_parser.text.sentence_window import SentenceWindowNodeParser
+from rag.node_parser.text.token import TokenTextSplitter
+from rag.node_parser.loading import load_parser
+
+from rag.prompt.chat_template import (
+    TEXT_QA_PROMPT_TMPL_MSGS,
+    TEXT_QA_SYSTEM_PROMPT,
+    TREE_SUMMARIZE_PROMPT_TMPL_MSGS,
+    CHAT_TEXT_QA_PROMPT,
+    CHAT_TREE_SUMMARIZE_PROMPT,
+    ChatPromptTemplate
 )
-
-from rag.node.base_node import (
-    BaseNode,
-    TextNode,
-    IndexNode,
-    NodeWithScore,
-    Document,
-    NodeRelationship,
-)
-from rag.node.types import (
-    ObjectType,
-    MetadataMode,
-    RelatedNodeInfo,
-    RelatedNodeType,
+from rag.prompt.prompt_template import PromptTemplate 
+from rag.prompt.selector_template import (
+    SelectorPromptTemplate,
+    DEFAULT_TEXT_QA_PROMPT_SEL,
+    DEFAULT_TREE_SUMMARIZE_PROMPT_SEL
 )
 
-from rag.node_parser.base import NodeParser, MetadataAwareTextSplitter, TextSplitter
+from rag.reader.directory_reader import DirectoryReader
+from rag.reader.file_reader.pdf_reader import  PDFMinerLoader, PyPDFReader
 
-from rag.prompt.base_prompt import BasePromptTemplate
-from rag.prompt.mixin import (
-    PromptMixin, 
-    HasPromptType,
-    PromptDictType, 
-    PromptMixinType
-)
-from rag.prompt.types import PromptType
+from rag.retrievers.auto_merging import AutoMergingRetriever
+from rag.retrievers.recursive import RecursiveRetriever
+from rag.retrievers.bm25 import BM25Retriever
 
-from rag.reader.base_reader import BaseReader
+from rag.storage.chat_store.simple_chat_store import SimpleChatStore
+from rag.storage.docstore.kv_docstore import KVDocumentStore
+from rag.storage.docstore.simple_docstore import SimpleDocumentStore
+from rag.storage.index_store.kv_index_store import KVIndexStore
+from rag.storage.index_store.simple_index_store import SimpleIndexStore
+from rag.storage.kv_store.simple_kvstore import SimpleKVStore
 
-from rag.retrievers.base_retriver import BaseRetriever
-from rag.retrievers.types import QueryBundle, QueryType
+from rag.synthesizer.summary import SimpleSummarize
+from rag.synthesizer.tree_summary import TreeSummarize
+from rag.synthesizer.generation import Generation
+from rag.synthesizer.no_text import NoText
+from rag.synthesizer.mode import ResponseMode
 
-from rag.synthesizer.base_synthesizer import BaseSynthesizer
-
-from rag.vector_stores.base_vector import VectorStore
-from rag.vector_stores.utils import metadata_dict_to_node, node_to_metadata_dict, legacy_metadata_dict_to_node
-from rag.vector_stores.types import (
-    VectorStoreQuery, 
-    VectorStoreQueryMode, 
-    VectorStoreQueryResult,
-    MetadataFilters, 
-    ExactMatchFilter,
-)
-
-from rag.engine.base_query_engine import BaseQueryEngine
-
-from rag.schema.output_parser import BaseOutputParser, PydanticProgramMode, BasePydanticProgram
-
-from rag.schema.component import BaseComponent, TransformComponent
-
+from rag.vector_stores.simple import SimpleVectorStore
+from rag.vector_stores.milvus import MilvusVectorStore
 
 __all__ = [
-    "CallbackManager", 
-    "CBEventType", 
-    "EventPayload",
-    "BaseEmbedding", 
-    "Pooling", 
-    "load_embedding", 
-    "save_embedding",
-    "BaseIndex",
-    "IndexDict",
-    "KeywordTable",
-    "IndexList",
-    "EmptyIndexStruct",
-    "IndexStructType",
-    "BaseChatStore",
-    "BaseDocumentStore",
-    "BaseIndexStore",
-    "BaseInMemoryKVStore",
-    "BaseKVStore",
-    "LLM",
-    "LLMType",
-    "messages_to_prompt",
-    "prompt_to_messages",
-    "llm_chat_callback",
-    "llm_completion_callback",
-    "IndexStructType",
-    "BaseNode",
-    "TextNode",
-    "IndexNode",
-    "NodeWithScore",
-    "Document",
-    "NodeRelationship",
-    "ObjectType",
-    "MetadataMode",
-    "RelatedNodeInfo",
-    "RelatedNodeType",
-    "NodeParser", 
-    "MetadataAwareTextSplitter", 
-    "TextSplitter",
-    "BasePromptTemplate",
-    "PromptMixin", 
-    "HasPromptType",
-    "PromptDictType", 
-    "PromptMixinType",
-    "PromptType",
-    "BaseReader",
-    "BaseRetriever",
-    "QueryBundle",
-    "QueryType",
-    "BaseSynthesizer",
-    "VectorStore",
-    "VectorStoreQuery",
-    "VectorStoreQueryMode",
-    "VectorStoreQueryResult",
-    "MetadataFilters",
-    "ExactMatchFilter",
-    "metadata_dict_to_node",
-    "node_to_metadata_dict",
-    "legacy_metadata_dict_to_node",
-    "BaseQueryEngine",
-    "BaseOutputParser",
-    "PydanticProgramMode",
-    "BasePydanticProgram",
-    "BaseComponent",
-    "TransformComponent",
+    "IngestionCache",
+    "HuggingFaceEmbedding",
+    "HuggingFaceInferenceAPIEmbedding",
+    "HuggingFaceInferenceAPIEmbeddings",
+    "MockEmbedding",
+    "resolve_embed_model",
+    "resolve_llm",
+    "LlamaCPP",
+    "HuggingFaceInferenceAPI",
+    "HuggingFaceLLM",
+    "MockLLM",
+    "load_parser",
+    "HierarchicalNodeParser",
+    "SentenceSplitter",
+    "SentenceWindowNodeParser",
+    "TokenTextSplitter",
+    "TEXT_QA_PROMPT_TMPL_MSGS",
+    "TEXT_QA_SYSTEM_PROMPT",
+    "TREE_SUMMARIZE_PROMPT_TMPL_MSGS",
+    "CHAT_TEXT_QA_PROMPT",
+    "CHAT_TREE_SUMMARIZE_PROMPT",
+    "ChatPromptTemplate",
+    "PromptTemplate",
+    "SelectorPromptTemplate",
+    "DEFAULT_TEXT_QA_PROMPT_SEL",
+    "DEFAULT_TREE_SUMMARIZE_PROMPT_SEL",
+    "DirectoryReader",
+    "PDFMinerLoader",
+    "PyPDFReader",
+    "AutoMergingRetriever",
+    "RecursiveRetriever",
+    "BM25Retriever",
+    "SimpleKVStore",
+    "KVIndexStore",
+    "SimpleIndexStore",    
+    "KVDocumentStore",
+    "SimpleDocumentStore",  
+    "SimpleChatStore",
+    "SimpleSummarize",
+    "TreeSummarize",
+    "Generation",
+    "NoText",
+    "ResponseMode",
+    "SimpleVectorStore",
+    "MilvusVectorStore"
 ]
