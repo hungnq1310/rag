2bfaeab4269f8739c03a14ae1b1d79ef3ad7a1b9
Hung Nguyen <hungnq131002@gmail.com>
2024-02-21 13:52:35 +0700

    feat: use config file store all paramater instead of splitting 2 type

--- rag/config/configuration.py
+++ rag/config/configuration.py
@@ -6,29 +6,24 @@ from rag.rag_utils.files import (
 )
 from .schema import (
     MilvusConfig,
-    MilvusArguments,
-    NodeParserArguments,
-    NodeParserConfig,
-    LlmParams,
-    EmbeddingsParams,
-    IndexRetrieveParams,
-    ResponseParams
+    SpiltterConfig,
+    LlmConfig,
+    EmbeddingConfig,
+    IndexRetrieverConfig,
+    ResponseConfig,
 )
 
 class ConfigurationManager: 
     def __init__(
-                self, 
-                config_filepath: Path = CONFIG_FILE_PATH, 
-                param_filepath: Path = PARAMS_FILE_PATH
-                ) -> None:
+        self, 
+        config_filepath: Path = CONFIG_FILE_PATH, 
+    ) -> None:
         self.config = read_yaml(config_filepath)
-        self.param = read_yaml(param_filepath)
-
         create_directories([self.config.artifacts_root])
     
     def get_milvus_config(self) -> MilvusConfig: 
         """create instace for data ingestion config"""
-        configs = self.config.milvus_config
+        configs = self.config.vector_store_config
 
         milvus_config = MilvusConfig(
             host= configs.host,
@@ -36,110 +31,101 @@ class ConfigurationManager:
             address= configs.address,
             uri= configs.uri,
             user= configs.user,
+            collection_name=configs.collection_name,
+            consistency_level=configs.consistency_level,
+            index_config=configs.index_config,
+            search_config=configs.search_config,
+            overwrite=configs.overwrite,
+            primary_field=configs.primary_field,
+            text_field=configs.text_field,
+            embedding_field=configs.embedding_field,
+            embedding_dim=configs.embedding_dim,
         )
         return milvus_config
-    
-    def get_milvus_params(self) -> MilvusArguments:
-        params = self.param.MilvusParams
-        milvus_params = MilvusArguments(
-            collection_name=params.collection_name,
-            consistency_level=params.consistency_level,
-            index_params=params.index_params,
-            search_params=params.search_params,
-            overwrite=params.overwrite,
-            primary_field=params.primary_field,
-            text_field=params.text_field,
-            embedding_field=params.embedding_field,
-            embedding_dim=params.embedding_dim,
-        )
-        return milvus_params
-    
-    def get_node_parser_config(self) -> NodeParserConfig: 
-        config = self.config.node_parser_config
-        node_parser_config = NodeParserConfig(
-            name=config.name
-        )
-        return node_parser_config
 
-    def get_node_parser_params(self) -> NodeParserArguments: 
-        params = self.param.NodeParserParams
-        node_parser_params = NodeParserArguments(
-            model_name_tokenizer=params.model_name_tokenizer,
-            splitter_mode=params.splitter_mode,
-            separator=params.separator,
-            chunk_size=params.chunk_size,
-            chunk_overlap=params.chunk_overlap,
-            paragraph_separator=params.paragraph_separator,
-            secondary_chunking_regex=params.secondary_chunking_regex,
-            backup_separators=params.backup_separators,
-        )
-        return node_parser_params
-            
-
-    def get_llm_params(self) -> LlmParams:
-        params = self.param.LlmParams
-        llm_params = LlmParams(
-            context_window=params.context_window,
-            max_new_tokens=params.max_new_tokens,
-            model_name=params.model_name,
-            tokenizer_name=params.tokenizer_name,
-            device_map=params.device_map,
-            top_k=params.top_k,
-            top_p=params.top_p,
-            temperature=params.temperature,
-            length_penalty=params.length_penalty,
-            repetition_penalty=params.repetition_penalty,
-            num_beams=params.num_beams,
-            do_sample=params.do_sample,
-            pelnaty_alpha=params.pelnaty_alpha,
-            use_cache=params.use_cache,
-            num_return_sequences=params.num_return_sequences,
-            pad_token_id=params.pad_token_id,
-            bos_token_id=params.bos_token_id,
-            eos_token_id=params.eos_token_id,
-        )
-        return llm_params
     
-    def get_embed_params(self) -> EmbeddingsParams:
-        params = self.param.embeddings_params
-        embed_params = EmbeddingsParams(
-            model_name= params.model_name,
-            tokenizer_name= params.tokenizer_name,
-            pooling= params.pooling,
-            max_length= params.max_length,
-            normalize= params.normalize,
-            embedding_batch_size= params.embedding_batch_size,
-            cache_folder= params.cache_folder,
-            trust_remote_code= params.trust_remote_code,
+    def get_splitter_config(self) -> SpiltterConfig: 
+        configs = self.config.splitter_config
+        splitter_config = SpiltterConfig(
+            model_name_tokenizer=configs.model_name_tokenizer,
+            splitter_mode=configs.splitter_mode,
+            separator=configs.separator,
+            chunk_size=configs.chunk_size,
+            chunk_overlap=configs.chunk_overlap,
+            paragraph_separator=configs.paragraph_separator,
+            secondary_chunking_regex=configs.secondary_chunking_regex,
+            backup_separators=configs.backup_separators,
         )
-        return embed_params
+        return splitter_config
 
-    def get_index_retriever_params(self) -> IndexRetrieveParams:
-        params = self.param.IndexRetrieveParams
-        embed_params = IndexRetrieveParams(
-            index_type=params.index_type,
-            similarity_top_k=params.similarity_top_k,
-            sparse_top_k=params.sparse_top_k,
-            alpha=params.alpha,
-            list_query_mode=params.list_query_mode,
-            keyword_table_mode=params.keyword_table_mode,
-            max_keywords_per_chunk=params.max_keywords_per_chunk,
-            max_keywords_per_query=params.max_keywords_per_query,
-            num_chunks_per_query=params.num_chunks_per_query,
-            vector_store_query_mode=params.vector_store_query_mode,
-            store_nodes_override=params.store_nodes_override,
-            insert_batch_size=params.insert_batch_size,
-            use_async=params.use_async,
-            show_progress=params.show_progress,
+
+    def get_llm_config(self) -> LlmConfig:
+        config = self.config.llm_config
+        llm_config = LlmConfig(
+            context_window=config.context_window,
+            max_new_tokens=config.max_new_tokens,
+            model_name=config.model_name,
+            tokenizer_name=config.tokenizer_name,
+            device_map=config.device_map,
+            top_k=config.top_k,
+            top_p=config.top_p,
+            temperature=config.temperature,
+            length_penalty=config.length_penalty,
+            repetition_penalty=config.repetition_penalty,
+            num_beams=config.num_beams,
+            do_sample=config.do_sample,
+            pelnaty_alpha=config.pelnaty_alpha,
+            use_cache=config.use_cache,
+            num_return_sequences=config.num_return_sequences,
+            pad_token_id=config.pad_token_id,
+            bos_token_id=config.bos_token_id,
+            eos_token_id=config.eos_token_id,
         )
-        return embed_params
+        return llm_config
     
-    def get_response_params(self) -> ResponseParams:
-        params = self.param.ResponseParams
-        response_params = ResponseParams(
-            verbose=params.verbose,
-            response_mode=params.response_mode,
-            use_async=params.use_async,
-            streaming=params.streaming,
+    def get_embed_config(self) -> EmbeddingConfig:
+        config = self.config.embeddings_config
+        embed_config = EmbeddingConfig(
+            model_name= config.model_name,
+            tokenizer_name= config.tokenizer_name,
+            pooling= config.pooling,
+            max_length= config.max_length,
+            normalize= config.normalize,
+            embedding_batch_size= config.embedding_batch_size,
+            cache_folder= config.cache_folder,
+            trust_remote_code= config.trust_remote_code,
         )
-        return response_params
+        return embed_config
+
+    def get_index_retriever_config(self) -> IndexRetrieverConfig:
+        config = self.config.index_retriever_config
+        embed_config = IndexRetrieverConfig(
+            index_type=config.index_type,
+            similarity_top_k=config.similarity_top_k,
+            sparse_top_k=config.sparse_top_k,
+            alpha=config.alpha,
+            list_query_mode=config.list_query_mode,
+            keyword_table_mode=config.keyword_table_mode,
+            max_keywords_per_chunk=config.max_keywords_per_chunk,
+            max_keywords_per_query=config.max_keywords_per_query,
+            num_chunks_per_query=config.num_chunks_per_query,
+            vector_store_query_mode=config.vector_store_query_mode,
+            store_nodes_override=config.store_nodes_override,
+            insert_batch_size=config.insert_batch_size,
+            use_async=config.use_async,
+            show_progress=config.show_progress,
+        )
+        return embed_config
+    
+    def get_response_config(self) -> ResponseConfig:
+        config = self.config.response_config
+        response_config = ResponseConfig(
+            verbose=config.verbose,
+            response_mode=config.response_mode,
+            use_async=config.use_async,
+            streaming=config.streaming,
+        )
+        return response_config
+    
+    def get_extract_adobe_pdf(self):
+        return self.config.extract_adobe_pdf
