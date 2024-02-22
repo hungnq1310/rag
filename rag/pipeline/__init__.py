c17bebe764c4b624c49b0501b5dbfed1fd294520
Hung Nguyen <hungnq131002@gmail.com>
2024-02-23 02:08:51 +0700

    chore: modify init file

--- rag/pipeline/__init__.py
+++ rag/pipeline/__init__.py
@@ -1,4 +1,13 @@
-from .retriever_pipeline import *
-from .node_parser import *
-from .decompose_pipeline import *
-from .rewrite_pipeline import *
+from .bm25_pipeline import BM25Pipeline
+from .decompose_milvus import MilvusSubquetionPipeline
+from .milvus_retriever import MilvusRetrieverPipeline
+from .simple_keyword_table import SimplKeywordeTablePipeline
+from .parsing_pipeline import ParsingPipeline
+
+__all__ = [
+    "BM25Pipeline",
+    "MilvusSubquetionPipeline",
+    "MilvusRetrieverPipeline",
+    "SimplKeywordeTablePipeline",
+    "ParsingPipeline",
+]
