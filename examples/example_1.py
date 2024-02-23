c22deb79550179f1adbec34fe17f9862b7ae78b4
Hung Nguyen <hungnq131002@gmail.com>
2024-02-23 16:45:59 +0700

    chore: change name to get more example

--- examples/example_1.py
+++ examples/example_1.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import time
+import os
 
-from rag.pipeline.retriever_pipeline import RetrieverPipeline
+from rag.pipeline.milvus_retriever import MilvusRetrieverPipeline
 from rag.config.configuration import ConfigurationManager
 
 # tạo database 
@@ -12,7 +13,7 @@ import glob
 
 startTime_load = int(round(time.time() * 1000))
 
-pdf_files = glob.glob("/home/hungnq/hungnq_2/rag_pdf/rag_pdf_services/data/*.pdf")
+pdf_files = glob.glob(os.path.abspath(os.curdir) + "/data/*.pdf")
 print("pdf_files: ", pdf_files)
 reader = DirectoryReader(
     input_files=pdf_files
@@ -40,35 +41,8 @@ index_retriver_config = manager.get_index_retriever_config()
 response_config = manager.get_response_config()
 
 
-
-# # llm
-# llm = HuggingFaceLLM(
-#     context_window=llm_params.context_window,
-#     max_new_tokens=llm_params.max_new_tokens,
-#     tokenizer_name=llm_params.tokenizer_name,
-#     model_name=llm_params.model_name,
-#     device_map=llm_params.device_map,
-#     generate_kwargs={
-#         "top_p": llm_params.top_p,
-#         "top_k": llm_params.top_k,
-#         "temperature": llm_params.temperature,
-#         "length_penalty": llm_params.length_penalty,
-#         "repetition_penalty": llm_params.repetition_penalty,
-#         "num_beams": llm_params.num_beams,
-#         "do_sample" : llm_params.do_sample,
-#         "pelnaty_alpha": llm_params.pelnaty_alpha,
-#         "use_cache": llm_params.use_cache,
-#         "num_return_sequences": llm_params.num_return_sequences,
-#         "pad_token_id": llm_params.pad_token_id,
-#         "bos_token_id": llm_params.bos_token_id,
-#         "eos_token_id": llm_params.eos_token_id
-#     }
-# )
-
-
 # cần 2 params and config: của milvus và index -> done
-
-retriever_pipeline = RetrieverPipeline(
+retriever_pipeline = MilvusRetrieverPipeline(
     splitter_config=node_parser_config,
     milvus_config=milvus_config,
     index_retriver_config=index_retriver_config,
@@ -88,6 +62,7 @@ while True:
         documents=pdf_documents,
     )
     for node in nodes:
+        print("-" * 20)
         text = node.get_content().strip()
         metadata_info = node.node.get_metadata_str()
         score = node.score
