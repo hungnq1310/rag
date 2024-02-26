8a8ee1a5c1da806f1cfb7435931c421eb5336bea
Hung Nguyen <hungnq131002@gmail.com>
2024-02-26 16:00:24 +0700

    fix: missing root path

--- rag/retrievers/__init__.py
+++ rag/retrievers/__init__.py
@@ -1,10 +1,10 @@
-from retrievers.sparse.auto_merging import AutoMergingRetriever
-from retrievers.sparse.recursive import RecursiveRetriever
-from retrievers.sparse.bm25 import BM25Retriever
-from retrievers.dense.emtpy_retriever import EmptyIndexRetriever
-from retrievers.dense.keyword_retriever import BaseKeywordTableRetriever
-from retrievers.dense.list_retriever import SummaryIndexEmbeddingRetriever, SummaryIndexLLMRetriever
-from retrievers.dense.vector_retriver import VectorIndexRetriever
+from rag.retrievers.sparse.auto_merging import AutoMergingRetriever
+from rag.retrievers.sparse.recursive import RecursiveRetriever
+from rag.retrievers.sparse.bm25 import BM25Retriever
+from rag.retrievers.dense.emtpy_retriever import EmptyIndexRetriever
+from rag.retrievers.dense.keyword_retriever import BaseKeywordTableRetriever
+from rag.retrievers.dense.list_retriever import SummaryIndexEmbeddingRetriever, SummaryIndexLLMRetriever
+from rag.retrievers.dense.vector_retriver import VectorIndexRetriever
 
 __all__ = [
     "AutoMergingRetriever",
