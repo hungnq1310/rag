a464e71d097fbcb896086448c37f7dbba64b59af
Hung Nguyen <hungnq131002@gmail.com>
2024-03-14 16:02:29 +0700

    feat: add two feature to read file for reader

---
+++ rag/reader/file/txt.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from src.reader.base_reader import BaseReader
+from src.node.base_node import Document
+
+
+class TxtReader(BaseReader):
+    """ Txt parser """
+
+    def load_data(self, file: Path, extra_info=None) -> Document:
+        # load_data returns a list of Document objects
+        with open(file, "r") as f:
+            text = f.read()
+        metadata = {"file_name": file.name}
+        if extra_info is not None:
+            metadata.update(extra_info)
+        return Document(text=text, extra_info=metadata)
