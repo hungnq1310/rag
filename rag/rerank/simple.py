3134d44448b1010937b1852795a973dfe471840f
Hung Nguyen <hungnq131002@gmail.com>
2024-03-21 15:30:04 +0700

    feat: add mean delta similarity post processor

--- rag/rerank/simple.py
+++ rag/rerank/simple.py
@@ -92,3 +92,40 @@ class DeltaSimilarityPostprocessor(BaseNodePostprocessor):
                 new_nodes.append(node)
 
         return new_nodes
+    
+class MeanDeltaSimilarityPostprocessor(BaseNodePostprocessor):
+    """Similarity-based Node processor."""
+
+    delta_similarity_cutoff: float = Field(default=None)
+
+    @classmethod
+    def class_name(cls) -> str:
+        return "SimilarityPostprocessor"
+
+    def _postprocess_nodes(
+        self,
+        nodes: List[NodeWithScore],
+        query_bundle: Optional[QueryBundle] = None,
+    ) -> List[NodeWithScore]:
+        """Postprocess nodes."""
+        sim_cutoff_exists = self.delta_similarity_cutoff is not None
+
+        # get mean score of all retrieved node
+        mean_score: float = 0.0
+        if nodes:
+            mean_score = sum([cast(float, node.score) for node in nodes]) / len(nodes)
+        
+        new_nodes = []
+        for node in nodes:
+            should_use_node = True
+            if sim_cutoff_exists:
+                similarity = node.score
+                if similarity is None:
+                    should_use_node = False
+                elif cast(float, similarity) - mean_score > self.delta_similarity_cutoff:
+                    should_use_node = False
+
+            if should_use_node:
+                new_nodes.append(node)
+
+        return new_nodes
