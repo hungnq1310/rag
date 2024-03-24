c89f00242f5716022772e13889f5fdc036216424
Hung Nguyen <hungnq131002@gmail.com>
2024-03-24 16:03:33 +0700

    feat: apply singleton pattern for storage context

--- rag/core/storage_context.py
+++ rag/core/storage_context.py
@@ -45,9 +45,16 @@ class StorageContext:
 
     """
 
+    _instance = None
+
     docstore: BaseDocumentStore
     index_store: BaseIndexStore
     vector_stores: Dict[str, VectorStore]
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls, *args, **kwargs)
+        return cls._instance
 
     @classmethod
     def from_defaults(
