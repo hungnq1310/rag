3145b300b69f95b1abf536a052edde35268b5813
Hung Nguyen <hungnq131002@gmail.com>
2024-03-24 15:35:38 +0700

    chore: only having model_name_or_path or embed model

--- rag/embeddings/resovle_embed.py
+++ rag/embeddings/resovle_embed.py
@@ -13,15 +13,13 @@ def resolve_embed_model(embed_model: Optional["EmbedType"] = None) -> "BaseEmbed
     """Resolve embed model."""
 
     if isinstance(embed_model, str):
-        splits = embed_model.split(":", 1)
-        model_name = splits[1] if len(splits) > 1 else splits
-
+        # cache
         cache_folder = os.path.join(get_cache_dir(), "models")
         os.makedirs(cache_folder, exist_ok=True)
-
+        
         print("using mock embeddings!!")
         embed_model = HuggingFaceEmbedding(
-            model_name=model_name, cache_folder=cache_folder
+            model_name=embed_model, cache_folder=cache_folder
         )
 
 
