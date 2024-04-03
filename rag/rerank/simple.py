2a8035db4309f7c6d0cb6c9c2ffbfd0e276268d9
Hung Nguyen <hungnq131002@gmail.com>
2024-04-03 16:37:29 +0700

    feat: add mean score filter

--- rag/rerank/simple.py
+++ rag/rerank/simple.py
@@ -133,3 +133,39 @@ class MeanDeltaSimilarityPostprocessor(BaseNodePostprocessor):
                 new_nodes.append(node)
 
         return new_nodes
+    
+
+class MeanSimilarityPostprocessor(BaseNodePostprocessor):
+
+    @classmethod
+    def class_name(cls) -> str:
+        return "MeanSimilarityPostprocessor"
+
+    def _postprocess_nodes(
+        self,
+        nodes: List[NodeWithScore],
+        query_bundle: Optional[QueryBundle] = None,
+    ) -> List[NodeWithScore]:
+        """Postprocess nodes."""
+
+        # get mean score of all retrieved node
+        mean_score: float = 0.0
+        if nodes:
+            mean_score = sum([cast(float, node.score) for node in nodes]) / len(nodes)
+        
+        new_nodes = []
+        for node in nodes:
+            should_use_node = True
+            # get similiarity score
+            similarity = node.score
+            if similarity is None:
+                should_use_node = False
+            similarity = cast(float, similarity)
+            # compare similarity score with mean score
+            if similarity < mean_score:
+                should_use_node = False
+
+            if should_use_node:
+                new_nodes.append(node)
+
+        return new_nodes
